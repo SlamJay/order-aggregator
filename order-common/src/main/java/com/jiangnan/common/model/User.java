4e29ae2298841f722c1daaea1a2e9f9b62ae1970
lijie <772141834@qq.com>
2018-11-16 00:04:47 +0800

    创建maven聚合项目

---
+++ order-common/src/main/java/com/jiangnan/common/model/User.java
@@ -0,0 +1,58 @@
+package com.jiangnan.common.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1828031108681954487L;
+
+    private  Long id;
+
+    private String name;
+
+    private String password;
+
+    private  String mobile;
+
+    private Integer sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+}
